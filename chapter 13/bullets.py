58c513b72ee9403fa50cad5c4ad23c86883df86b
MaxSchleper <mx.schleper@gmail.com>
2018-06-24 13:26:04 +0200

    lab12

--- chapter 13/bullets.py
+++ chapter 13/bullets.py
@@ -15,7 +15,17 @@ SCREEN_HEIGHT = 400
 
 ''' Classes '''
 
-''' Game class '''
+class Block(pygame.sprite.Sprite):
+    # this class represents a block
+    def __init__(self, color):
+        super.__init__()
+
+        self.image = pygame.Surface([20, 15])
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+
+''' Game Class '''
 
 class Game():
     ''' Attributes '''
@@ -37,6 +47,19 @@ class Game():
         self.block_list = pygame.sprite.Group()
         self.all_sprites_list = pygame.sprite.Group()
 
+        # Create the block sprites
+        for i in range(50):
+            block = Block(BLACK, 20, 15)
+
+            block.rect.x = random.randrange(SCREEN_WIDTH - 20)
+            block.rect.y = random.randrange(SCREEN_HEIGHT- 15)
+
+            self.block_list.add(block)
+            self.all_sprites_list.add(block)
+
+        # Create the player
+        self.player = Player(RED, 20, 15)
+        self.all_sprites_list.add(self.player)
 
     # closing window and restarting game
     def process_events(self):
@@ -56,6 +79,16 @@ class Game():
             # move all the sprites
             self.all_sprites_list.update()
 
+            # see if player collided with anything
+            blocks_hit_list = pygame.sprite.spritecollide(self.player, self.block_list, True)
+
+            #check list for collisions
+            for block in blocks_hit_list:
+                self.score += 1
+                print(self.score)
+
+            if len(self.block_list) == 0:
+                self.game_over = True
 
     def display_frame(self, screen):
         screen.fill(WHITE)
