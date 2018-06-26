b115d15439a09f1b06743bd82705d7ae756045ad
P0STMAN <mx.schleper@gmail.com>
2018-06-26 19:51:47 +0200

    13b game over screen text splitting

--- chapter 13/bullets.py
+++ chapter 13/bullets.py
@@ -9,6 +9,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 400
@@ -18,12 +19,37 @@ SCREEN_HEIGHT = 400
 class Block(pygame.sprite.Sprite):
     # this class represents a block
     def __init__(self, color):
-        super.__init__()
+        super().__init__()                     # super()
 
         self.image = pygame.Surface([20, 15])
         self.image.fill(color)
 
         self.rect = self.image.get_rect()
+
+class Player(pygame.sprite.Sprite):
+    # this class represents the player
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([20, 20])
+        self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.x = pos[0]
+        self.rect.y = SCREEN_HEIGHT - 30
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([4, 10])
+        self.image.fill(BLACK)
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.y -= 6
 
 ''' Game Class '''
 
@@ -46,20 +72,21 @@ class Game():
         # create sprite lists
         self.block_list = pygame.sprite.Group()
         self.all_sprites_list = pygame.sprite.Group()
+        self.bullet_list = pygame.sprite.Group()
 
         # Create the block sprites
-        for i in range(50):
-            block = Block(BLACK, 20, 15)
+        for i in range(5):
+            self.block = Block(BLUE)
 
-            block.rect.x = random.randrange(SCREEN_WIDTH - 20)
-            block.rect.y = random.randrange(SCREEN_HEIGHT- 15)
+            self.block.rect.x = random.randrange(SCREEN_WIDTH - 20)
+            self.block.rect.y = random.randrange(250)
 
-            self.block_list.add(block)
-            self.all_sprites_list.add(block)
+            self.block_list.add(self.block)
+            self.all_sprites_list.add(self.block)
 
-        # Create the player
-        self.player = Player(RED, 20, 15)
+        self.player = Player()
         self.all_sprites_list.add(self.player)
+
 
     # closing window and restarting game
     def process_events(self):
@@ -69,6 +96,14 @@ class Game():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.game_over:
                     self.__init__()
+                else:
+                    # fire a bullet
+                    bullet = Bullet()
+                    # set the bullet where the player is
+                    bullet.rect.x = self.player.rect.x
+                    bullet.rect.y = self.player.rect.y
+                    self.all_sprites_list.add(bullet)
+                    self.bullet_list.add(bullet)
 
         return False
 
@@ -79,27 +114,38 @@ class Game():
             # move all the sprites
             self.all_sprites_list.update()
 
-            # see if player collided with anything
-            blocks_hit_list = pygame.sprite.spritecollide(self.player, self.block_list, True)
+            # bullets mechanics
+            for bullet in self.bullet_list:
+                block_hit_list = pygame.sprite.spritecollide(bullet, self.block_list, True)
 
-            #check list for collisions
-            for block in blocks_hit_list:
-                self.score += 1
-                print(self.score)
+                for block in block_hit_list:
+                    self.bullet_list.remove(bullet)
+                    self.all_sprites_list.remove(bullet)
+                    self.score += 1
+                    print(self.score)
 
             if len(self.block_list) == 0:
                 self.game_over = True
+
+
+
 
     def display_frame(self, screen):
         screen.fill(WHITE)
 
         if self.game_over:
             # display a text in the middle of the screen
+            font = pygame.font.SysFont("serif", 45)
+            text1 = font.render("Game Over!", True, BLACK)
+            x = (SCREEN_WIDTH // 2) - (text1.get_width() // 2)
+            y = (SCREEN_HEIGHT // 2) - (text1.get_height() // 2) - 25
+            screen.blit(text1, [x, y])
+
             font = pygame.font.SysFont("serif", 25)
-            text = font.render("Game Over, click to restart", True, BLACK)
-            x = (SCREEN_WIDTH // 2) - (text.get_width() // 2)
-            y = (SCREEN_HEIGHT // 2) - (text.get_height() // 2)
-            screen.blit(text, [x, y])
+            text2 = font.render("Click to restart", True, BLACK)
+            x = (SCREEN_WIDTH / 2) - (text2.get_width() // 2)
+            y = (SCREEN_HEIGHT // 2) - (text2.get_height() // 2) + 25
+            screen.blit(text2, [x, y])
         # if game is not over
         else:
             self.all_sprites_list.draw(screen)
